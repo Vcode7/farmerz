528af881ce4ce14b9638b86de84f3425ff70a9ed
Vikas <vikassirvi656@gmail.com>
2024-09-15 16:24:24 +0530

    devloping

---
+++ src/app/charts/basic-chart/page.tsx
@@ -0,0 +1,22 @@
+
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLaout";
+import React from "react";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+
+export const metadata: Metadata = {
+  title: "Next.js Basic Chart Page | NextAdmin - Next.js Dashboard Kit",
+  description: "This is Next.js Basic Chart page for NextAdmin Dashboard Kit",
+  // other metadata
+};
+
+const BasicChartPage: React.FC = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Basic Chart" />
+
+    </DefaultLayout>
+  );
+};
+
+export default BasicChartPage;
